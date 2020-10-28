758f1f83f99383e5539175e245502fdc6e1d631b
philipheferreira <philipheferreira@gmail.com>
2020-10-28 15:00:03 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector('.container')
+const texto = document.querySelector('#texto')
+
+const tempoTotal = 7500
+const tempoRespiracao = (tempoTotal/5) *2
+const tempoPrenderRespiracao = tempoTotal / 5
+
+function animacaoRespiracao () { 
+    
+}
