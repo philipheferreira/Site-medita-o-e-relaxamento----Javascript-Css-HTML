eafcba7d4fe0b317bacd0fbf754f919bad6362c5
philipheferreira <philipheferreira@gmail.com>
2020-10-28 16:28:22 -0300

    Atualizar as funções referentes a funcionalidades e a base do html, acessando o arquivo javascript

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const tempoTotal = 7500
 const tempoRespiracao = (tempoTotal/5) *2
 const tempoPrenderRespiracao = tempoTotal / 5
 
+animacaoRespiracao()
+
+
 function animacaoRespiracao () { 
-    
+    texto.innerHTML = "Inspire fundo!!!"
+    container.className = "container cresce"
+
+    setTimeout(() => { 
+        texto.innerText = "Segure a respiração"
+
+        setTimeout(() => { 
+            texto.innerText = "Solte a respiração"
+            container.className = "container encolher"
+        }, tempoPrenderRespiracao)
+    }, tempoRespiracao)
 }
+
+setInterval(animacaoRespiracao, tempoTotal)
